4fa0878c4d398247d1b2c29eb8454268535387aa
Brayden Lopez <blopez@instructure.com>
2015-02-22 17:15:16 -0700

    Changing things to modals.

--- js/javas.js
+++ js/javas.js
@@ -1,21 +1,37 @@
 (function ($) {
     $(document).ready(function () {
-        $("#navbar-collapse-under, #navbar-collapse-over").click(function () {
-            $("#sidebar").toggleClass("opened");
+        var modalBG = $(".modal-background");
+        var currentModal = null;
+
+        $("#navbar-button").click(function () {
+            $("body").toggleClass("opened");
         });
         $("#Grid").mixitup();
+
+        modalBG.click(function() {
+            if (currentModal)
+                currentModal.hide();
+            modalBG.css({opacity: 0}).hide();
+        });
         
         $(".project .details").each(function() {
             var that = $(this);
             var newDetails = $("<div class='details'><a href='#'><i class='fa fa-chain'></i></a></div>");
-            $("a", newDetails).click(function() {
-                alert("this is where a popup would happen");
+            var closeButton = $("<a href='#' class='close clearfix'>Close</a>");
+            $("a", newDetails).click(function(e) {
+                e.preventDefault();
+                that.show();
+                currentModal = that;
+                modalBG.css({opacity:.5}).show();
+            });
+            closeButton.click(function(e) {
+                e.preventDefault();
+                that.hide();
+                modalBG.css({opacity: 0}).hide();
             });
             that.before(newDetails);
+            $(".links", that).append(closeButton);
             that.addClass('details-modal-data').removeClass('details').hide();
-        })
-
-
-
+        });
     });
 })(jQuery);
