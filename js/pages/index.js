8963bd6cb8c8b6c75d735961e4903a18c108f320
headdetect <headdetective2@gmail.com>
2018-09-04 16:20:30 -0600

    Updating...

--- js/pages/index.js
+++ js/pages/index.js
@@ -1,2 +1,2 @@
-const particles=Particles.init({selector:"#particles",maxParticles:100,connectParticles:!0,color:"#FFFFFF",responsive:[{breakpoint:768,options:{maxParticles:55}},{breakpoint:375,options:{maxParticles:30}}]});
+const particles=Particles.init({selector:"#particles",maxParticles:100,connectParticles:!0,color:"#FFFFFF",responsive:[{breakpoint:768,options:{maxParticles:55}},{breakpoint:425,options:{maxParticles:30}}]});
 //# sourceMappingURL=index.js.map
